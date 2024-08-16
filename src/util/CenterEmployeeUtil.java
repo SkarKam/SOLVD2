4a93a21211339912b68231fe0084723f32f3dfcd
GitGutPJ <s25020@pjwstk.edu.pl>
2024-08-16 16:20:11 +0200

    Repair util class

--- src/util/CenterEmployeeUtil.java
+++ src/util/CenterEmployeeUtil.java
@@ -9,18 +9,13 @@ public class CenterEmployeeUtil {
     private CenterEmployeeUtil() {}
 
     public static String getFullName(CenterEmployee centerEmployee){
-        if(centerEmployee.getClass() == CenterEmployee.class) {
+        {
             return centerEmployee.getName() + ' ' + centerEmployee.getSurname();
-        } else {
-            throw  new RuntimeException("This is not a Center Employee");
         }
     }
     public static String parseDateOfEmployement(CenterEmployee centerEmployee){
-        if(centerEmployee.getClass() == CenterEmployee.class) {
+
             DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
             return dateFormat.format(centerEmployee.getDateOfEmployment());
-        } else {
-            throw new RuntimeException("This is not a Center Employee");
-        }
     }
 }
