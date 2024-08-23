741f551e76a169bd0126dfe13d25d720235c717d
GitGutPJ <s25020@pjwstk.edu.pl>
2024-08-23 17:29:44 +0200

    Adding interfaces + final

--- SOLVD2/src/models/persons/employees/CenterEmployee.java
+++ SOLVD2/src/models/persons/employees/CenterEmployee.java
@@ -30,24 +30,24 @@ public abstract class CenterEmployee extends Person implements ICenterEmployee{
 
     @Override
     public boolean equals(Object o) {
-
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
         CenterEmployee that = (CenterEmployee) o;
-        return Objects.equals(name, that.name) && Objects.equals(surname, that.surname);
+        return Objects.equals(dateOfEmployment, that.dateOfEmployment);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, surname);
+        return Objects.hash(super.hashCode(), dateOfEmployment);
     }
 
     @Override
     public String toString() {
         return "CenterEmployee{" +
-                "name='" + name + '\'' +
+                "dateOfEmployment=" + dateOfEmployment +
+                ", name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
-                ", dateOfEmployment=" + dateOfEmployment +
                 '}';
     }
 }
